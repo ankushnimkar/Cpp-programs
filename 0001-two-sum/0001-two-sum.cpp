4bfeeb8c30fe5d9772c8b43a1e69b11eeaf51ed4
ankushnimkar <68778124+ankushnimkar@users.noreply.github.com>
2024-10-16 20:20:02 +0530

    Time: 8 ms (78.16%), Space: 16.1 MB (8.01%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -1,10 +1,12 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
+       //create a map store index of each array element
         unordered_map<int, int> mp;
         for(int index=0; index <nums.size(); ++index)
             mp[nums[index]] = index;
         
+        //check reminder is present in map and if found return the indeces
         for(int index=0; index <nums.size(); ++index)
         {
             int rem=target - nums[index];
